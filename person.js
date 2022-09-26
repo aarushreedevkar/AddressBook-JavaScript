e92a4e17f34dd912ccfc1dd5e247ec968cfd614d
jyoti <jyotidhage10@gmail.com>
2022-09-26 18:56:50 +0530

    Aaradhya completed JavaScript Programming constructs Assignment

---
+++ person.js
@@ -0,0 +1,48 @@
+  class Person{
+    constructor(firstName,lastName,address,city,state,zip,phoneNumber,Email){
+        this.firstName = firstName;
+        this.lastName=lastName;
+        this.address=address;
+        this.city= city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.Email = Email;
+    }
+    getName(){
+        return this.firstName = Aaradhya;
+ }
+    getLastName(){
+        return this.lastName = Devkar;
+    }
+    getaddress(){
+        return this.address = Hadapsar;
+ }
+    getCity(){
+        return this.city = Pune;
+    }
+    getState(){
+        return this.state = Maharashtra;
+    }
+    getzip(){
+        return this.zip = 412308;
+ }
+    getphoeNumber(){
+        return this.phoneNumber = 9673683178;
+    }
+    getEmail(){
+        return this.Email ="jyoti@gmail.com"
+    }
+  }
+  console.log(person.getName());
+  console.log(person.getLastName());
+  console.log(person.getaddress());
+  console.log(person.getcity());
+  console.log(person.getState());
+  console.log(person.getzip());
+  console.log(person.getphoeNumber());
+  console.log(person.getEmail());
+  
+  
+
+    
